c5d4f581ed6ee753e62e67f39f86f1cf66b621ba
Graham Lowe <graham.lowe@gmail.com>
2016-05-24 12:39:19 -0400

    Use use-package.

--- init.el
+++ init.el
@@ -1,109 +1,139 @@
-;;;;;;;;;;;;;;
-;; Requires ;;
-;;;;;;;;;;;;;;
+;;; package -- Summary
+;;;
 
-(add-to-list 'load-path "~/.emacs.d/lib")
+;;; Commentary:
 
-(require 'cc-mode)
-(require 'cl)
-(require 'package)
-(require 'xcscope)
+;;; Code:
 
-;; The following packages are installed via package
-(package-initialize)
-(require 'go-mode)
-(require 'color-theme)
+;; Key-bindings
 
-
-;;;;;;;;;;;;;;;;
-;; Path setup ;;
-;;;;;;;;;;;;;;;;
-
-(defun read-file-lines (file)
-  "Return a list of lines in FILE."
-  (with-temp-buffer
-    (condition-case nil (insert-file-contents file) (file-error nil))
-    (split-string
-     (buffer-string) "\n" t)))
-
-;; Prepend paths in /etc/paths.d/ to our PATH environment variable.
-(let* ((paths-dir (file-name-as-directory "/etc/paths.d"))
-       (paths-dir-list1 (condition-case nil (directory-files paths-dir) (file-error nil)))
-       (paths-dir-list2 (remove-if #'(lambda (s) (or (string= s ".") (string= s ".."))) paths-dir-list1))
-       (paths-filenames (mapcar #'(lambda (s) (concat paths-dir s)) paths-dir-list2))
-       (paths (apply #'append (mapcar #'read-file-lines paths-filenames)))
-       (paths (nconc paths (list (getenv "PATH"))))
-       (paths (nconc paths (read-file-lines "/etc/paths")))
-       (path-string (mapconcat #'identity paths ":")))
-  (setenv "PATH" path-string))
-
-;;;;;;;;;;;;;;;;;;;;
-;; Customizations ;;
-;;;;;;;;;;;;;;;;;;;;
-
-(add-hook 'before-save-hook #'gofmt-before-save)
-
-;; Experiment: See if disabling backups and autosave files helps
-;; minimize the frequency with which emacs locks up.
-(setq backup-inhibited t)
-(setq auto-save-default nil)
-
-(add-to-list
- 'package-archives
- '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; Enable downcase a region
-(put 'downcase-region 'disabled nil)
-;; For tunnel vision superpowers.
-(put 'narrow-to-region 'disabled nil)
-;; Enable viewing images in emacs.
-(put 'image-toggle-display 'disabled nil)
-;; Fontify all buffers
-(global-font-lock-mode 1)
-;; maximum colors
-(setq font-lock-maximum-decoration t)
-;; Always use transient marks
-(transient-mark-mode 1)
-(toggle-uniquify-buffer-names)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-;; This is essential to ensure that hard links are broken when saved.
-(setq file-precious-flag t)
-(menu-bar-mode 1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(setq-default truncate-lines nil)
-(setq truncate-partial-width-windows nil)
-(setq column-number-mode t)
-(setq line-number-mode t)
-(setq vc-stay-local nil)
-(setq dired-recursive-deletes 'top)
-(ansi-color-for-comint-mode-on)
-(put 'upcase-region 'disabled nil)
-
-;;;;;;;;;;;;;;
-;; Bindings ;;
-;;;;;;;;;;;;;;
-
-;; Keep the fingers on the Control key is superior than using meta
+;; This saves me from having to contort my fingers to reach Meta-x.
 (define-key global-map (kbd "C-x C-m") 'execute-extended-command)
 ;; Override the default key binding: only delete ws _ahead_ of point.
 (define-key global-map (kbd "M-\\") 'c-hungry-delete-forward)
-(define-key global-map (kbd "RET") 'newline-and-indent)
-(define-key global-map (kbd "C-x C-a") 'kmacro-call-macro)
 
-;;;;;;;;;;;;;
-;; Startup ;;
-;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-light)))
- '(custom-safe-themes (quote ("d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#fdf6e3" :foreground "#657b83" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Source Code Pro")))))
+;; Setup `package'.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Bootstrap `use-package'.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :if (and (eq system-type 'darwin) (display-graphic-p))
+  :init (exec-path-from-shell-initialize))
+
+(use-package osx-trash
+  :if (eq system-type 'darwin)
+  :ensure t
+  :init (osx-trash-setup))
+
+(use-package flycheck
+  :ensure t
+  :no-require t
+  :init (global-flycheck-mode)
+  :config
+  (flycheck-define-checker proselint
+    "A linter for prose."
+    :command ("proselint" source-inplace)
+    :error-patterns
+    ((warning line-start (file-name) ":" line ":" column ": "
+              (id (one-or-more (not (any " "))))
+              (message (one-or-more not-newline)
+                       (zero-or-more "\n"
+                                     (any " ")
+                                     (one-or-more not-newline)))
+              line-end))
+    :modes (text-mode markdown-mode gfm-mode))
+  (add-to-list 'flycheck-checkers 'proselint)
+  :diminish (flycheck-mode . " Ⓢ"))
+
+(use-package whitespace
+  :ensure t
+  :init (progn
+          (setq whitespace-style '(face trailing tabs tab-mark lines-tail))
+          (add-hook 'prog-mode-hook 'whitespace-mode))
+  :config  (setq whitespace-line-column 80)
+  :diminish (whitespace-mode . " Ⓦ"))
+
+(use-package typo                       ; Automatically use typographic quotes
+  :ensure t
+  :init (progn
+          (typo-global-mode)
+          (dolist (hook '(markdown-mode-hook
+                          rst-mode-hook
+                          text-mode-hook))
+            (add-hook hook 'typo-mode)))
+  :diminish (typo-mode . " Ⓣ"))
+
+(use-package jedi
+  :ensure t
+  :init (add-hook 'python-mode 'jedi:setup)
+  :config (setq jedi:complete-on-dot t))
+
+(use-package magit
+  :ensure t)
+
+(use-package helm
+  :ensure t
+  :init (helm-mode 1)
+  :bind (
+         ("C-x C-f" . helm-find-files)
+         ("C-x b" . helm-buffers-list)
+         ("C-x m" . helm-M-x)
+         ("M-y" . helm-show-kill-ring)
+         ("C-x r b" . helm-bookmarks)
+         (:map helm-mode-map
+               "<tab>" . helm-execute-persistent-action))
+  :diminish helm-mode)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+(use-package rust-mode
+  :ensure t
+  :init (add-hook 'rust-mode-hook
+                  (lambda ()
+                    (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+  :diminish rust-mode)
+
+(use-package cargo
+  :ensure t
+  :init (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
+(use-package yasnippet
+  :ensure t)
+
+(use-package nand2tetris
+  :ensure t)
+
+(use-package flycheck-rust
+  :ensure t
+  :init (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(use-package racer
+  :ensure t
+  :config (progn
+            (setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
+            (setq racer-rust-src-path "/Users/gl/Code/rust/src") ;; Rust source code PATH
+            )
+  :init (progn
+          (add-hook 'rust-mode-hook #'racer-mode)
+          (add-hook 'racer-mode-hook #'eldoc-mode)
+          (add-hook 'racer-mode-hook #'company-mode)))
+
+(set-frame-font "Input Mono-14")
+(setq column-number-mode t)
+(setq dired-recursive-deletes 'top)
+(tool-bar-mode -1)
+(setq-default indent-tabs-mode nil)
+
+(provide 'init)
+;;; init.el ends here
+(put 'narrow-to-region 'disabled nil)
